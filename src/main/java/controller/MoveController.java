221a8491febbc03ffec351eca6338bdef8ff328f
YaroslavKuchmiy <tislenko.yv@gmail.com>
2017-07-27 19:14:42 +0100

    Create view/ConsoleView and controller/MoveController

---
+++ src/main/java/controller/MoveController.java
@@ -0,0 +1,7 @@
+package controller;
+
+/**
+ * Created by Yaroslav on 27.07.2017.
+ */
+public class MoveController {
+}
---
+++ src/main/java/view/ConsoleView.java
@@ -0,0 +1,7 @@
+package view;
+
+/**
+ * Created by Yaroslav on 27.07.2017.
+ */
+public class ConsoleView {
+}
