81caa7037f7ae1ce4748225dc9cacb98864577bc
YaroslavKuchmiy <tislenko.yv@gmail.com>
2017-07-28 18:56:40 +0300

    Create inner class GameBuilder

--- src/main/java/model/Game.java
+++ src/main/java/model/Game.java
@@ -8,11 +8,11 @@ public class Game {
     private final String player2;
     private final Field field;
 
-    public Game(final String name, final String player1, final String player2, final Field field) {
-        this.name = name;
-        this.player1 = player1;
-        this.player2 = player2;
-        this.field = field;
+    public Game(final GameBuilder gameBuilder) {
+        this.name = gameBuilder.getName();
+        this.player1 = gameBuilder.getPlayer1();
+        this.player2 = gameBuilder.getPlayer2();
+        this.field = gameBuilder.getField();
     }
 
     public String getName() {
@@ -30,4 +30,48 @@ public class Game {
     public Field getField() {
         return field;
     }
+
+    public class GameBuilder {
+
+        public String name;
+        public String player1;
+        public String player2;
+        public Field field;
+
+        public GameBuilder name(String name){
+            this.name = name;
+            return this;
+        }
+        public GameBuilder player1(String player1){
+            this.player1 = player1;
+            return this;
+        }
+        public GameBuilder player2(String player2){
+            this.player2 = player2;
+            return this;
+        }
+        public GameBuilder field(Field field){
+            this.field = field;
+            return this;
+        }
+
+        public String getName(){
+            return this.name;
+        }
+        public String getPlayer1(){
+            return this.player1;
+        }
+        public String getPlayer2(){
+            return this.player2;
+        }
+        public Field getField(){
+            return this.field;
+        }
+
+        public Game build(){
+            return new Game(this);
+        }
+
+    }
+
 }
