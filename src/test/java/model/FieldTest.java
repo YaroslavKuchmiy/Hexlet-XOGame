5db6a037974740a312aba4b273d0fdba64f1d3eb
YaroslavKuchmiy <tislenko.yv@gmail.com>
2017-07-28 20:40:52 +0300

    Edit FieldTest

--- src/test/java/model/FieldTest.java
+++ src/test/java/model/FieldTest.java
@@ -4,9 +4,6 @@ import org.junit.Test;
 
 import static org.junit.Assert.*;
 
-/**
- * Created by Yaroslav on 28.07.2017.
- */
 public class FieldTest {
     @Test
     public void getSize() throws Exception {
