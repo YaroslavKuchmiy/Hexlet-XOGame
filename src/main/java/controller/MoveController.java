05ac035ef5ce04de00ffea35f0aa2352af8225b4
YaroslavKuchmiy <tislenko.yv@gmail.com>
2017-07-29 17:55:09 +0300

    Create MoveController

--- src/main/java/controller/MoveController.java
+++ src/main/java/controller/MoveController.java
@@ -1,7 +1,20 @@
 package controller;
 
-/**
- * Created by Yaroslav on 27.07.2017.
- */
+
+import model.Field;
+import model.Figure;
+import model.Point;
+import model.exception.AlreadyOccupiedException;
+
 public class MoveController {
+
+    public void applyFigure(final Field field,
+                            final Point point,
+                            final Figure figure) throws AlreadyOccupiedException {
+        if (field.getFigure(point) != null){
+            throw new AlreadyOccupiedException();
+        }
+        field.setFigure(figure, point);
+    }
+
 }
