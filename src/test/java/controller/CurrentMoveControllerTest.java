d377ea7797ed27b844c7578143abb55e31bbd93b
YaroslavKuchmiy <tislenko.yv@gmail.com>
2017-07-30 13:15:58 +0300

    Create CurrentMoveControllerTest

--- src/test/java/controller/CurrentMoveControllerTest.java
+++ src/test/java/controller/CurrentMoveControllerTest.java
@@ -1,8 +1,55 @@
+package controller;
+
+import model.Field;
+import model.Figure;
+import model.Point;
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
-/**
- * Created by Yaroslav on 30.07.2017.
- */
+
 public class CurrentMoveControllerTest {
+    @Test
+    public void currentMoveNull() throws Exception {
+        final Field field = new Field();
+        final CurrentMoveController checkMove = new CurrentMoveController();
+
+        for (int x = 0; x < field.getSize(); x++) {
+            for (int y = 0; y < field.getSize(); y++) {
+                field.setFigure(Figure.X, new Point(x, y));
+            }
+        }
+
+        assertNull(checkMove.currentMove(field));
+    }
+
+    @Test
+    public void currentMoveFigureX() throws Exception {
+        final Figure expectValue = Figure.X;
+
+        final Field field = new Field();
+        final CurrentMoveController checkMove = new CurrentMoveController();
+
+        field.setFigure(Figure.X, new Point(0, 1));
+        field.setFigure(Figure.O, new Point(1, 0));
+        field.setFigure(Figure.X, new Point(1, 1));
+        field.setFigure(Figure.O, new Point(2, 1));
+
+        assertEquals(expectValue, checkMove.currentMove(field));
+    }
+
+    @Test
+    public void currentMoveFigureO() throws Exception {
+        final Figure expectValue = Figure.O;
+
+        final Field field = new Field();
+        final CurrentMoveController checkMove = new CurrentMoveController();
+
+        field.setFigure(Figure.X, new Point(0, 1));
+        field.setFigure(Figure.O, new Point(1, 0));
+        field.setFigure(Figure.X, new Point(1, 1));
+
+        assertEquals(expectValue, checkMove.currentMove(field));
+    }
 
 }
