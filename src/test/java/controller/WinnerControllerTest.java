a1d241ef17dabe118e1c3eaa28eb7a7e9f285774
YaroslavKuchmiy <tislenko.yv@gmail.com>
2017-07-30 12:11:45 +0300

    Edit WinnerControllerTest - check null and check all column and row

--- src/test/java/controller/WinnerControllerTest.java
+++ src/test/java/controller/WinnerControllerTest.java
@@ -7,9 +7,7 @@ import org.junit.Test;
 
 import static org.junit.Assert.*;
 
-/**
- * Created by Yaroslav on 29.07.2017.
- */
+
 public class WinnerControllerTest {
     @Test
     public void getWinnerNull() throws Exception {
@@ -24,14 +22,31 @@ public class WinnerControllerTest {
         final Figure inputValue = Figure.X;
         final Figure expectValue = inputValue;
 
-        final Field field = new Field();
+        Field field = new Field();
         final WinnerController win = new WinnerController();
 
-        for (int i = 0; i < field.getSize(); i++){
-            field.setFigure(inputValue, new Point(1, i));
+        for (int x = 0; x < field.getSize(); x++) {
+            field = new Field();
+            for (int i = 0; i < field.getSize(); i++) {
+                field.setFigure(inputValue, new Point(x, i));
+            }
+            assertEquals(expectValue, win.getWinner(field));
         }
+    }
 
-        assertEquals(expectValue, win.getWinner(field));
+    @Test
+    public void getWinnerCheckRowNull() throws Exception {
+        Field field = new Field();
+        final WinnerController win = new WinnerController();
+
+        for (int i = 0; i < field.getSize(); i++) {
+            field = new Field();
+            field.setFigure(Figure.X, new Point(i, 0));
+            field.setFigure(Figure.O, new Point(i, 1));
+            field.setFigure(Figure.X, new Point(i, 2));
+
+            assertNull(win.getWinner(field));
+        }
     }
 
     @Test
@@ -39,14 +54,31 @@ public class WinnerControllerTest {
         final Figure inputValue = Figure.X;
         final Figure expectValue = inputValue;
 
-        final Field field = new Field();
+        Field field = new Field();
         final WinnerController win = new WinnerController();
 
-        for (int i = 0; i < field.getSize(); i++){
-            field.setFigure(inputValue, new Point(i, 1));
+        for (int x = 0; x < field.getSize(); x++) {
+            field = new Field();
+            for (int i = 0; i < field.getSize(); i++) {
+                field.setFigure(inputValue, new Point(i, x));
+            }
+            assertEquals(expectValue, win.getWinner(field));
         }
+    }
 
-        assertEquals(expectValue, win.getWinner(field));
+    @Test
+    public void getWinnerCheckColumnNull() throws Exception {
+        Field field = new Field();
+        final WinnerController win = new WinnerController();
+
+        for (int i = 0; i < field.getSize(); i++) {
+            field = new Field();
+            field.setFigure(Figure.X, new Point(0, i));
+            field.setFigure(Figure.O, new Point(1, i));
+            field.setFigure(Figure.X, new Point(2, i));
+
+            assertNull(win.getWinner(field));
+        }
     }
 
     @Test
@@ -64,6 +96,20 @@ public class WinnerControllerTest {
         assertEquals(expectValue, win.getWinner(field));
     }
 
+    @Test
+    public void getWinnerCheckDiag1Null() throws Exception {
+        Field field = new Field();
+        final WinnerController win = new WinnerController();
+
+        field.setFigure(Figure.X, new Point(0, 0));
+        field.setFigure(Figure.O, new Point(1, 1));
+        field.setFigure(Figure.X, new Point(1, 1));
+
+        assertNull(win.getWinner(field));
+
+    }
+
+    @Test
     public void getWinnerCheckDiag2() throws Exception {
         final Figure inputValue = Figure.X;
         final Figure expectValue = inputValue;
@@ -78,4 +124,16 @@ public class WinnerControllerTest {
         assertEquals(expectValue, win.getWinner(field));
     }
 
+    @Test
+    public void getWinnerCheckDiag2Null() throws Exception {
+        Field field = new Field();
+        final WinnerController win = new WinnerController();
+
+        field.setFigure(Figure.X, new Point(0, 2));
+        field.setFigure(Figure.O, new Point(1, 1));
+        field.setFigure(Figure.X, new Point(2, 0));
+
+        assertNull(win.getWinner(field));
+
+    }
 }
