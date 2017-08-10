5e5845e01f5b14d58cb09fd9f0325bf1726ee9da
YaroslavKuchmiy <tislenko.yv@gmail.com>
2017-08-10 19:07:41 +0300

    Edit Field - add InvalidPointException

--- src/main/java/model/Field.java
+++ src/main/java/model/Field.java
@@ -1,8 +1,11 @@
 package model;
 
 
+import model.exception.InvalidPointException;
+
 public class Field {
 
+    private static final int MIN_COORDINATE = 0;
     private final int sizeField = 3;
     private final Figure[][] figures = new Figure[sizeField][sizeField];
 
@@ -10,12 +13,20 @@ public class Field {
         return figures.length;
     }
 
-    public Figure getFigure(final Point point) {
+    public Figure getFigure(final Point point) throws InvalidPointException {
+        checkPoint(point);
         return figures[point.getX()][point.getY()];
     }
 
-    public void setFigure (final Figure figure, final Point point) {
+    public void setFigure (final Figure figure, final Point point) throws InvalidPointException {
+        checkPoint(point);
         figures[point.getX()][point.getY()] = figure;
     }
 
+    private void checkPoint (final Point point) throws InvalidPointException {
+        if (point.getX() < MIN_COORDINATE || point.getY() < MIN_COORDINATE
+                || point.getX() >= sizeField || point.getY() >= sizeField){
+            throw new InvalidPointException();
+        }
+    }
 }
