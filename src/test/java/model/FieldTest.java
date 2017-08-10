f942ae74e975749ed31fcbebcec4c4e48d0fc943
YaroslavKuchmiy <tislenko.yv@gmail.com>
2017-08-10 19:39:12 +0300

    Edit FieldTest - InvalidPointException

--- src/test/java/model/FieldTest.java
+++ src/test/java/model/FieldTest.java
@@ -1,5 +1,6 @@
 package model;
 
+import model.exception.InvalidPointException;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -25,6 +26,98 @@ public class FieldTest {
         field.setFigure(inputValue,point);
 
         assertEquals(expectedValue,field.getFigure(point));
+    }
+
+    @Test
+    public void getFigureCheckPointMaxX() throws Exception {
+        final Field field = new Field();
+        final Point point = new Point(3, 0);
+
+        try {
+            field.getFigure(point);
+            fail();
+        } catch (InvalidPointException e){}
+    }
+
+    @Test
+    public void getFigureCheckPointMinX() throws Exception {
+        final Field field = new Field();
+        final Point point = new Point(-1, 0);
+
+        try {
+            field.getFigure(point);
+            fail();
+        } catch (InvalidPointException e){}
+    }
+
+    @Test
+    public void getFigureCheckPointMaxY() throws Exception {
+        final Field field = new Field();
+        final Point point = new Point(0, 3);
+
+        try {
+            field.getFigure(point);
+            fail();
+        } catch (InvalidPointException e){}
+    }
+
+    @Test
+    public void getFigureCheckPointMinY() throws Exception {
+        final Field field = new Field();
+        final Point point = new Point(0, -1);
+
+        try {
+            field.getFigure(point);
+            fail();
+        } catch (InvalidPointException e){}
+    }
+
+    @Test
+    public void setFigureCheckPointMaxX() throws Exception {
+        final Figure figure = Figure.X;
+        final Field field = new Field();
+        final Point point = new Point(3, 0);
+
+        try {
+            field.setFigure(figure, point);
+            fail();
+        } catch (InvalidPointException e){}
+    }
+
+    @Test
+    public void setFigureCheckPointMinX() throws Exception {
+        final Figure figure = Figure.X;
+        final Field field = new Field();
+        final Point point = new Point(-1, 0);
+
+        try {
+            field.setFigure(figure, point);
+            fail();
+        } catch (InvalidPointException e){}
+    }
+
+    @Test
+    public void setFigureCheckPointMaxY() throws Exception {
+        final Figure figure = Figure.X;
+        final Field field = new Field();
+        final Point point = new Point(0, 3);
+
+        try {
+            field.setFigure(figure, point);
+            fail();
+        } catch (InvalidPointException e){}
+    }
+
+    @Test
+    public void setFigureCheckPointMinY() throws Exception {
+        final Figure figure = Figure.X;
+        final Field field = new Field();
+        final Point point = new Point(0, -1);
+
+        try {
+            field.setFigure(figure, point);
+            fail();
+        } catch (InvalidPointException e){}
     }
 
 }
