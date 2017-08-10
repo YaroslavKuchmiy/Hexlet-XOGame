72106677056da9875eca0961e940af30e25a1128
YaroslavKuchmiy <tislenko.yv@gmail.com>
2017-08-10 19:15:32 +0300

    Edit MoveController - add InvalidPointException

--- src/main/java/controller/MoveController.java
+++ src/main/java/controller/MoveController.java
@@ -5,12 +5,13 @@ import model.Field;
 import model.Figure;
 import model.Point;
 import model.exception.AlreadyOccupiedException;
+import model.exception.InvalidPointException;
 
 public class MoveController {
 
     public void applyFigure(final Field field,
                             final Point point,
-                            final Figure figure) throws AlreadyOccupiedException {
+                            final Figure figure) throws AlreadyOccupiedException, InvalidPointException {
         if (field.getFigure(point) != null){
             throw new AlreadyOccupiedException();
         }
